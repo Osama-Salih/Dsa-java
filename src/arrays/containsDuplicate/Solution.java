d0ef318727761874c44f4cbe5db8f2f5e49a6624
Osama Salih <osamasalih.ca@gmail.com>
2025-04-23 15:06:07 +0300

    Contains Duplicate

---
+++ src/arrays/containsDuplicate/Solution.java
@@ -0,0 +1,17 @@
+package arrays.containsDuplicate;
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
