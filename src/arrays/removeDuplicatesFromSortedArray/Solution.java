f6c3e75a664b7c929d5efb626e89dac090a97cfb
Osama Salih <osamasalih.ca@gmail.com>
2025-05-18 12:57:53 +0300

    Remove Duplicates from Sorted Array

---
+++ src/arrays/removeDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,14 @@
+package arrays.removeDuplicatesFromSortedArray;
+
+public class Solution {
+    public int removeDuplicates(int []nums) {
+        int insertIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+             nums[insertIndex] = nums[i];
+             insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+}
