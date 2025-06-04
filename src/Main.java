5170c64d3f73c618d602fa3a4c203b4ee0d7724b
Osama Salih <osamasalih.ca@gmail.com>
2025-06-04 11:45:15 +0300

    Top K Frequent Elements

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,40 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        int nums[] = new int[]{1,1,1,2,2,2,3};
+        int k = 2;
+
+        topKFrequent(nums, k);
     }
-}
+
+    public static void topKFrequent(int nums[], int k) {
+       if (nums.length == k) {
+           return nums;
+       }
+
+       Map<Integer, Integer> count = new HashMap<>();
+       for (int n: nums) {
+           count.put(n, count.getOrDefault(n, 0) + 1);
+       }
+
+       Queue<Integer> heap = new PriorityQueue<>(
+               (a, b) -> count.get(a) - count.get(b)
+       );
+
+       for (int n : count.keySet()) {
+           heap.add(n);
+           if (heap.size() > k) {
+               heap.poll();
+           }
+       }
+
+       int ans[] = new int[k];
+       for (int i = 0; i < k; i++) {
+           ans[i] = heap.poll();
+       }
+
+       return ans;
+    }
+  }
+
