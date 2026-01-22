bd2896cb8218d8e16873caaeb5a8339dc8add5bb
Osama Salih <osamasalih.ca@gmail.com>
2026-01-22 20:51:31 +0300

    304. Range Sum Query 2D - Immutable

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import arrays.rangeSumQuery2DImmutable.NumMatrix;
+
 public class Main {
     public static void main(String[] args) {
+        int matrix[][] = {{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
+        NumMatrix obj = new NumMatrix(matrix);
+        System.out.println(obj.sumRegion(2, 1, 4, 3));
     }
 }
