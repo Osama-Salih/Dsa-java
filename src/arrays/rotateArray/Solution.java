872ea60b467949bcea855a892beab3c7dcb77567
Osama Salih <osamasalih.ca@gmail.com>
2026-01-28 20:07:47 +0300

    189. Rotate Array

---
+++ src/arrays/rotateArray/Solution.java
@@ -0,0 +1,21 @@
+package arrays.rotateArray;
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length - 1;
+        k %= n;
+
+        reverse(nums, 0, n);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n);
+    }
+    private void reverse(int []nums, int l, int r) {
+        while(l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
