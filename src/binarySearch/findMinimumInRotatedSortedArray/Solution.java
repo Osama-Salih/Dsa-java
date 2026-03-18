7df87d27aabac40a4e85b571ce9cc36ff197af39
Osama Salih <osamasalih.ca@gmail.com>
2026-03-18 03:02:42 +0300

    153. Find Minimum in Rotated Sorted Array

---
+++ src/binarySearch/findMinimumInRotatedSortedArray/Solution.java
@@ -0,0 +1,18 @@
+package binarySearch.findMinimumInRotatedSortedArray;
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while(l < r) {
+            int mid = l + ((r - l) / 2);
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
+// tc : O(log n)
+// sc : O(1)
