d30eeb77cb37c571afd6fcd1539278636742adce
Osama Salih <osamasalih.ca@gmail.com>
2026-03-23 21:54:17 +0300

    141. Linked List Cycle

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
 import linkedList.ListNode;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(4);
-
-        ListNode list2 = new ListNode(1);
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
-//        print(mergeTwoLists(list1, list2));
+//        ListNode list1 = new ListNode(1);
+//        list1.next = new ListNode(2);
+//        list1.next.next = list1;
+////        list1.next.next = new ListNode(0);
+////        list1.next.next.next = new ListNode(4);
+////        list1.next.next.next.next = list1.next;
+////        print(list1);
+//        System.out.println(hasCycle(list1));
     }
 
-    public static void print(ListNode list) {
-        ListNode cur = list;
-        while(cur != null) {
-            System.out.println(cur.val);
-            cur = cur.next;
-        }
-    }
+
+
+//    public static void print(ListNode list) {
+//        ListNode cur = list;
+//        while(cur != null) {
+//            System.out.println(cur.val);
+//            cur = cur.next;
+//        }
+//    }
 }
