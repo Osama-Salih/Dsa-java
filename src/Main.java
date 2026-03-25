9fbde3b4047ff0796a4d9c039dc11a85ddc49654
Osama Salih <osamasalih.ca@gmail.com>
2026-03-25 11:21:50 +0300

    143. Reorder List

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 import linkedList.ListNode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 //        ListNode list1 = new ListNode(1);
 //        list1.next = new ListNode(2);
-//        list1.next.next = list1;
+//        list1.next.next = new ListNode(3);
+//        list1.next.next.next = new ListNode(4);
 ////        list1.next.next = new ListNode(0);
 ////        list1.next.next.next = new ListNode(4);
 ////        list1.next.next.next.next = list1.next;
 ////        print(list1);
 //        System.out.println(hasCycle(list1));
+//        print(reorderList(list1));
+//        System.out.println(Arrays.toString(reorderList(list1)));
+//       print(reorderList(list1));
     }
-
 
 
 //    public static void print(ListNode list) {
