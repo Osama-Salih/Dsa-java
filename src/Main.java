2170976dd63ac56299bbdebb58578d9adfc366d8
Osama Salih <osamasalih.ca@gmail.com>
2026-03-26 23:27:22 +0300

    138. Copy List with Random Pointer

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,21 @@
 import linkedList.ListNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(3);
-        list1.next.next.next = new ListNode(4);
-        list1.next.next.next.next = new ListNode(5);
-//        list1.next.next.next = new ListNode(4);
-////        list1.next.next.next.next = list1.next;
-////        print(list1);
-//        System.out.println(hasCycle(list1));
-//        print(reorderList(list1));
-//        System.out.println(Arrays.toString(reorderList(list1)));
-//       print(reorderList(list1));
-//        print(removeNthFromEnd(list1, 2));
+//        Node list1 = new Node(1);
+//        list1.next = new Node(2);
+//        list1.random = new Node(1);
+//        list1.next.random = new Node(1);
+//        list1.next.next = null;
+//        print(copyRandomList(list1));
+//        copyRandomList(list1);
+
     }
 
-
-//    public static void print(ListNode list) {
-//        ListNode cur = list;
+//    public static void print(Node list) {
+//        Node cur = list;
 //        while(cur != null) {
 //            System.out.println(cur.val);
 //            cur = cur.next;
---
+++ src/linkedList/copyListWithRandomPointer/Solution.java
@@ -0,0 +1,47 @@
+package linkedList.copyListWithRandomPointer;
+
+public class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Node cur = head;
+        while(cur != null) {
+            Node newNode = new Node(cur.val);
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = newNode.next;
+        }
+
+        cur = head;
+        while(cur != null) {
+            if (cur.random != null)
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        Node newHead = head.next;
+        Node newCur = newHead;
+        while(cur != null) {
+            cur.next = newCur.next;
+            cur = cur.next;
+            if (cur != null) {
+                newCur.next = cur.next;
+                newCur = newCur.next;
+            }
+        }
+        return newHead;
+    }
+
+    private static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
