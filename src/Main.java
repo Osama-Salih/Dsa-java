e09120205f53e24d953b77f996c5cbb31c21b3b9
Osama Salih <osamasalih.ca@gmail.com>
2026-03-28 17:14:21 +0300

    2. Add Two Numbers

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,57 @@ import linkedList.ListNode;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-//        Node list1 = new Node(1);
-//        list1.next = new Node(2);
-//        list1.random = new Node(1);
-//        list1.next.random = new Node(1);
-//        list1.next.next = null;
-//        print(copyRandomList(list1));
-//        copyRandomList(list1);
+    public static void main(String[] args){
+        ListNode list1 = new ListNode(9);
+        list1.next = new ListNode(9);
+        list1.next.next = new ListNode(9);
+        list1.next.next.next = new ListNode(9);
+        list1.next.next.next.next = new ListNode(9);
+//        list1.next.next.next.next.next = new ListNode(9);
+//        list1.next.next.next.next.next.next = new ListNode(9);
 
+        ListNode list2 = new ListNode(9);
+        list2.next = new ListNode(9);
+        list2.next.next = new ListNode(9);
+//        list2.next.next.next = new ListNode(9);
+        print(addTwoNumbers(list1, list2));
     }
 
-//    public static void print(Node list) {
-//        Node cur = list;
-//        while(cur != null) {
-//            System.out.println(cur.val);
-//            cur = cur.next;
-//        }
-//    }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode results = new ListNode(0);
+        ListNode ptr = results;
+        int carry = 0;
+
+        while(l1 != null || l2 != null) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
+        }
+        if (carry == 1) ptr.next = new ListNode(1);
+        return results.next;
+    }
+
+
+
+
+    public static void print(ListNode list) {
+        ListNode cur = list;
+        while(cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+    }
 }
