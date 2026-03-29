b107a8b1bba338eb316c792e2d394debe74bb30c
Osama Salih <osamasalih.ca@gmail.com>
2026-03-29 14:34:42 +0300

    287. Find the Duplicate Number

--- src/Main.java
+++ src/Main.java
@@ -4,56 +4,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        ListNode list1 = new ListNode(9);
-        list1.next = new ListNode(9);
-        list1.next.next = new ListNode(9);
-        list1.next.next.next = new ListNode(9);
-        list1.next.next.next.next = new ListNode(9);
+//        ListNode list1 = new ListNode(9);
+//        list1.next = new ListNode(9);
+//        list1.next.next = new ListNode(9);
+//        list1.next.next.next = new ListNode(9);
+//        list1.next.next.next.next = new ListNode(9);
 //        list1.next.next.next.next.next = new ListNode(9);
 //        list1.next.next.next.next.next.next = new ListNode(9);
 
-        ListNode list2 = new ListNode(9);
-        list2.next = new ListNode(9);
-        list2.next.next = new ListNode(9);
-//        list2.next.next.next = new ListNode(9);
-        print(addTwoNumbers(list1, list2));
-    }
-
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode results = new ListNode(0);
-        ListNode ptr = results;
-        int carry = 0;
-
-        while(l1 != null || l2 != null) {
-            int sum = carry;
-
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-
-            carry = sum / 10;
-            sum = sum % 10;
-            ptr.next = new ListNode(sum);
-            ptr = ptr.next;
-        }
-        if (carry == 1) ptr.next = new ListNode(1);
-        return results.next;
+//        ListNode list2 = new ListNode(9);
+//        list2.next = new ListNode(9);
+//        list2.next.next = new ListNode(9);
+////        list2.next.next.next = new ListNode(9);
+//        print(list1);
+//        System.out.println(findDuplicate(new int[]{1,3,4,2,2}));
     }
 
 
 
-
-    public static void print(ListNode list) {
-        ListNode cur = list;
-        while(cur != null) {
-            System.out.print(cur.val + " ");
-            cur = cur.next;
-        }
-    }
+//    public static void print(ListNode list) {
+//        ListNode cur = list;
+//        while(cur != null) {
+//            System.out.print(cur.val + " ");
+//            cur = cur.next;
+//        }
+//    }
 }
