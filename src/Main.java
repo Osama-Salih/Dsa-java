da880d1097090e59d07bc29c44d16764242073ac
Osama Salih <osamasalih.ca@gmail.com>
2026-04-02 21:56:40 +0300

    146. LRU Cache

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import dsa.DoublyLinkedList;
 import linkedList.ListNode;
-import linkedList.designCircularQueue.MyCircularQueue;
+import linkedList.lRUCache.LRUCache;
+
 
 public class Main {
     public static void main(String[] args){
-        ListNode list1 = new ListNode(1);
+//        ListNode list1 = new ListNode(1);
 //        list1.next = new ListNode(2);
 //        list1.next.next = new ListNode(3);
 //        list1.next.next.next = new ListNode(4);
@@ -17,6 +19,15 @@ public class Main {
 ////        list2.next.next.next = new ListNode(9);
 //        print(reverseBetween(list1, 1, 3));
 //        System.out.println(findDuplicate(new int[]{1,3,4,2,2}));
+
+//        LRUCache lRUCache = new LRUCache(2);
+//        System.out.println(lRUCache.get(2));      // return -1
+//        lRUCache.put(2, 6);                      // cache: {2=6}
+//        System.out.println(lRUCache.get(1));      // return -1
+//        lRUCache.put(1, 5);                      // cache: {2=6, 1=5}
+//        lRUCache.put(1, 2);                      // cache: {2=6, 1=2} (update + most recent)
+//        System.out.println(lRUCache.get(1));      // return 2
+//        System.out.println(lRUCache.get(2));      // return 6
 
     }
 
