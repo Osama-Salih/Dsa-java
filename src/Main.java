556f6faa2119f7a3cf65666bc0e945d5df987ce1
Osama Salih <osamasalih.ca@gmail.com>
2026-05-07 17:22:49 +0300

    39. Combination Sum

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+//        System.out.println(combinationSum(new int[]{2, 3, 6, 7}, 7));
+//        System.out.println(combinationSum(new int[]{8,7,4,3}, 11));
+//        System.out.println(combinationSum(new int[]{2, 3, 5}, 8));
+//        System.out.println(combinationSum(new int[]{3, 5, 8}, 11));
+//        System.out.println(combinationSum(new int[]{2}, 1));
     }
 }
---
+++ src/backtracking/combinationSum/Solution.java
@@ -0,0 +1,35 @@
+package backtracking.combinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    private List<List<Integer>> res;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        res = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        dfs(0, new ArrayList<>(), 0, candidates, target);
+        return res;
+    }
+    private void dfs(int i, List<Integer> cur, int total, int[] nums, int target) {
+        if (total == target) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for (int j = i; j < nums.length; j++) {
+            if (total + nums[j] > target) {
+                return;
+            }
+            cur.add(nums[j]);
+            dfs(j, cur, total + nums[j], nums, target);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
+// t = O(N^(T/M))
+// s = O(T/M)
