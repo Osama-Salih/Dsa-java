fdf70d3f13d925ba6452aa24742cf5a7bafca8d0
Osama Salih <osamasalih.ca@gmail.com>
2026-05-24 15:15:49 +0300

    211. Design Add and Search Words Data Structure

---
+++ src/trie/WordDictionary.java
@@ -0,0 +1,55 @@
+package trie;
+
+public class WordDictionary {
+    private TrieNode root;
+
+    private static class TrieNode {
+        private TrieNode[] children;
+        private boolean isWord;
+
+        public TrieNode() {
+            this.children = new TrieNode[26];
+            this.isWord = false;
+        }
+    }
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                cur.children[c - 'a'] = new TrieNode();
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+    private boolean dfs(String word, int j, TrieNode root) {
+        TrieNode cur = root;
+
+        for (int i = j; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (c == '.') {
+                for (TrieNode child : cur.children) {
+                    if (child != null && dfs(word, i + 1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (cur.children[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.children[c - 'a'];
+            }
+        }
+        return cur.isWord;
+    }
+}
