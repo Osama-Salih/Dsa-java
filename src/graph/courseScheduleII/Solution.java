c9730aa25aecee2d829a719167be49573c392a36
Osama Salih <osamasalih.ca@gmail.com>
2026-06-16 17:12:15 +0300

    210. Course Schedule II

---
+++ src/graph/courseScheduleII/Solution.java
@@ -0,0 +1,47 @@
+package graph.courseScheduleII;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int[] ans = new int[numCourses];
+        int i = 0, courses = 0;
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            ans[i++] = node;
+            courses++;
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.offer(it);
+                }
+            }
+        }
+        return courses != numCourses ? new int[]{} : ans;
+    }
+}
+// t = O(V + E)
+// s = O(V)
